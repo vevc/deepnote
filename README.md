38ab6dc183401dc0fea5d8903ed3151f80260c9c
vevc <dahaimaomi@gmail.com>
2025-04-30 12:44:37 +0800

    Init project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Deepnote
+
+## Quick Start
+
+```bash
+export HOST=e2f6282f-f1c3-4f9f-87f6-5b5588f5411a.deepnoteproject.com
+curl https://raw.githubusercontent.com/vevc/deepnote/main/install.sh | sh
+```
+
---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+HOST=${HOST:-xxx.deepnoteproject.com}
+wget https://github.com/XTLS/Xray-core/releases/download/v25.3.6/Xray-linux-64.zip
+unzip Xray-linux-64.zip
+wget https://raw.githubusercontent.com/vevc/deepnote/main/config.json
+UUID=`xray uuid`
+sed -i 's/$UUID/'$UUID'/g' $PWD/config.json
+wget https://raw.githubusercontent.com/vevc/deepnote/main/startup.sh
+chmod +x startup.sh
+$PWD/startup.sh
+echo "vless://$UUID@$HOST:443?encryption=none&security=tls&fp=random&type=ws&path=%2F%3Fed%3D2560#deepnote-ws"
